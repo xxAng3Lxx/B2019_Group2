359a67e64db4b92d844cc7042607674a6c89d149
xxAng3Lxx <cccailer18@gmail.com>
2022-01-19 14:14:06 +0800

    01192022

---
+++ FinalProject/Student's Pad/app/src/main/java/com/example/loginregister/listeners/NotesListener.java
@@ -0,0 +1,7 @@
+package com.example.loginregister.listeners;
+
+import com.example.loginregister.entities.Note;
+
+public interface NotesListener {
+    void onNoteClicked(Note note, int position);
+}
